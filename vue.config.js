64a9ce89765fe8d23452c986323f90e2592ff2ad
Your Name <1094498919@qq.com>
2020-04-06 20:47:03 +0800

    路由配置

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = {
+    // devServer:{
+    //     host:'localhost',
+    //     post:'8080',
+    //     proxy:{
+    //         '/activity':{
+    //             target:'https://www.imooc.com',
+    //             changeOrigin:false
+    //         }
+    //     }
+    // }
+}
